628dd611dfbd69c379d02eacd8202df118376229
Mostafa Morovati <mostafaamorovvati@gmail.com>
2024-07-14 10:14:30 +0430

    feat : add user avatar  to uesr output

--- src/modules/user/dto/user.output.ts
+++ src/modules/user/dto/user.output.ts
@@ -1,39 +1,43 @@
-import { toPersianDate } from '@/common/middleware/to-persian-date.middleware';
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
+
+import { toPersianDate } from '@/common/middleware/to-persian-date.middleware';
 
 @InputType('UserOutputInputType', { isAbstract: true })
 @ObjectType('UserOutputType')
 export class UserOutput {
-  @Field(type => String)
+  @Field(() => String)
   _id: string;
 
-  @Field(type => String, { nullable: true })
+  @Field(() => String, { nullable: true })
   displayName?: string;
 
-  @Field(type => String, { nullable: true })
+  @Field(() => String, { nullable: true })
   email?: string;
 
-  @Field(type => String, { nullable: true })
+  @Field(() => String, { nullable: true })
   phone?: string;
 
-  @Field(type => Boolean, { nullable: true })
+  @Field(() => Boolean, { nullable: true })
   isVerified?: boolean;
 
-  @Field(type => [String])
+  @Field(() => [String])
   roles?: string[];
 
-  @Field(type => String, { nullable: true })
+  @Field(() => String, { nullable: true })
   country?: string;
 
-  @Field(type => String, { nullable: true })
+  @Field(() => String, { nullable: true })
   city?: string;
 
-  @Field(type => String, { nullable: true })
+  @Field(() => String, { nullable: true })
   address?: string;
 
-  @Field(type => String, { nullable: true, middleware: [toPersianDate] })
+  @Field(() => String, { nullable: true })
+  avatar?: string;
+
+  @Field(() => String, { nullable: true, middleware: [toPersianDate] })
   createdAt?: Date;
 
-  @Field(type => String, { nullable: true, middleware: [toPersianDate] })
+  @Field(() => String, { nullable: true, middleware: [toPersianDate] })
   updatedAt?: Date;
 }
